dfc9af16a76e5d34df40a986db1bc0a94b456d01
MadchaosPlaya <cpanda113@gmail.com>
2015-07-06 16:17:05 +0100

    Initial commit

---
+++ MvcMovie1/Controllers/HelloworldController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie1.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        //
+        // GET: /Helloworld/
+
+        public ActionResult Index()
+        {
+		    return View();
+	    }
+
+		//
+		// GET: / Helloworld/
+
+		public ActionResult Welcome(string Connor, int numtimes = 1)
+		{
+		    ViewBag.Message = "Hello " + Connor; 
+			ViewBag.Numtimes = numtimes;
+
+            return View();
+        }
+
+	}
+}
+
+
+	
+
+	
+
+
---
+++ MvcMovie1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcMovie1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Quality Assurance Agency for Higher Education")]
+[assembly: AssemblyProduct("MvcMovie1")]
+[assembly: AssemblyCopyright("Copyright © Quality Assurance Agency for Higher Education 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da8a62f3-3116-47c6-8249-ded6b6beb5ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
